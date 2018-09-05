6cfaf210a8f11f429a1e3d2eaa55abc7fc576d6c
shibayu36 <shibayu36@gmail.com>
2018-09-06 07:55:11 +0900

    ブランチにチェックアウトしてなかった

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ echo $BRANCH
 scp nginx/nginx.conf root@$IPADDR:/etc/nginx/nginx.conf
 scp nginx/sites-enabled/nginx.conf root@$IPADDR:/etc/nginx/sites-enabled/nginx.conf
 scp mysql/my.cnf root@$IPADDR:/etc/mysql/my.cnf
-ssh isucon@$IPADDR "source ~/.profile && source ~/.bashrc && cd /home/isucon/isubata && git pull && cd webapp/go && make && sudo systemctl restart mysql && sudo systemctl restart nginx && sudo sudo systemctl restart isubata.golang.service && sudo sysctl -p"
+ssh isucon@$IPADDR "source ~/.profile && source ~/.bashrc && cd /home/isucon/isubata && git fetch && git checkout -f origin/$BRANCH && cd webapp/go && make && sudo systemctl restart mysql && sudo systemctl restart nginx && sudo sudo systemctl restart isubata.golang.service && sudo sysctl -p"
 
-# ssh isucon@$IPADDR "source ~/.profile && source ~/.bashrc && cd /home/isucon/isubata && git pull && ~/.local/perl/bin/carton install && sudo systemctl restart mysql && sudo systemctl restart nginx && sudo sudo systemctl restart isubata.golang.service && sudo sysctl -p"
+# ssh isucon@$IPADDR "source ~/.profile && source ~/.bashrc && cd /home/isucon/isubata && git fetch && git checkout -f origin/$BRANCH && ~/.local/perl/bin/carton install && sudo systemctl restart mysql && sudo systemctl restart nginx && sudo sudo systemctl restart isubata.golang.service && sudo sysctl -p"
