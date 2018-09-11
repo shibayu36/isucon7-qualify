dfc97c826e984590556f1d88287f2f8d1e0e1e20
shibayu36 <shibayu36@gmail.com>
2018-09-11 09:04:54 +0900

    deploy.sh直す

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,11 @@ echo $BRANCH
 scp nginx/nginx.conf root@$IPADDR:/etc/nginx/nginx.conf
 scp nginx/sites-enabled/nginx.conf root@$IPADDR:/etc/nginx/sites-enabled/nginx.conf
 scp mysql/my.cnf root@$IPADDR:/etc/mysql/my.cnf
-ssh isucon@$IPADDR "source ~/.profile && source ~/.bashrc && cd /home/isucon/isubata && git fetch && git checkout -f origin/$BRANCH && cd webapp/go && make && sudo systemctl restart mysql && sudo systemctl restart nginx && sudo sudo systemctl restart isubata.golang.service && sudo sysctl -p"
+scp files/app/env.sh isucon@$IPADDR:/home/isucon/env.sh
 
-# ssh isucon@$IPADDR "source ~/.profile && source ~/.bashrc && cd /home/isucon/isubata && git fetch && git checkout -f origin/$BRANCH && ~/.local/perl/bin/carton install && sudo systemctl restart mysql && sudo systemctl restart nginx && sudo sudo systemctl restart isubata.golang.service && sudo sysctl -p"
+# scp files/app/isubata.perl.service root@$IPADDR:/etc/systemd/system/isubata.perl.service
+# ssh isucon@$IPADDR "sudo systemctl daemon-reload"
+
+# ssh isucon@$IPADDR "source ~/.profile && source ~/.bashrc && cd /home/isucon/isubata && git fetch && git checkout -f origin/$BRANCH && cd webapp/go && make && sudo systemctl restart mysql && sudo systemctl restart nginx && sudo sudo systemctl restart isubata.golang.service && sudo sysctl -p"
+
+ssh isucon@$IPADDR "source ~/.profile && source ~/.bashrc && cd /home/isucon/isubata/webapp/perl && git fetch && git checkout -f origin/$BRANCH && ~/local/perl/bin/carton install && sudo systemctl restart mysql && sudo systemctl restart nginx && sudo sudo systemctl restart isubata.perl.service && sudo sysctl -p"
