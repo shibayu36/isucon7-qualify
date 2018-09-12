016c219e4d3970d5f4a7a7e25883ab457fe8200e
shibayu36 <shibayu36@gmail.com>
2018-09-13 07:03:42 +0900

    deployをrsyncにしてみる

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,9 @@ scp files/app/env.sh isucon@$IPADDR:/home/isucon/env.sh
 # scp files/app/isubata.perl.service root@$IPADDR:/etc/systemd/system/isubata.perl.service
 # ssh isucon@$IPADDR "sudo systemctl daemon-reload"
 
-# ssh isucon@$IPADDR "source ~/.profile && source ~/.bashrc && cd /home/isucon/isubata && git fetch && git checkout -f origin/$BRANCH && cd webapp/go && make && sudo systemctl restart mysql && sudo systemctl restart nginx && sudo sudo systemctl restart isubata.golang.service && sudo sysctl -p"
+# --deleteしてもいいけど変なファイル消えてもあれなんで使わない
+rsync -avz --exclude-from=.gitignore --exclude='.git' -e 'ssh' . isucon@$IPADDR:/home/isucon/isubata/
 
-ssh isucon@$IPADDR "source ~/.profile && source ~/.bashrc && cd /home/isucon/isubata/webapp/perl && git fetch && git checkout -f origin/$BRANCH && ~/local/perl/bin/carton install && sudo systemctl restart mysql && sudo systemctl restart nginx && sudo sudo systemctl restart isubata.perl.service && sudo sysctl -p"
+# ssh isucon@$IPADDR "source ~/.profile && source ~/.bashrc && cd /home/isucon/isubata && cd webapp/go && make && sudo systemctl restart mysql && sudo systemctl restart nginx && sudo sudo systemctl restart isubata.golang.service && sudo sysctl -p"
+
+ssh isucon@$IPADDR "source ~/.profile && source ~/.bashrc && cd /home/isucon/isubata/webapp/perl && ~/local/perl/bin/carton install && sudo systemctl restart mysql && sudo systemctl restart nginx && sudo sudo systemctl restart isubata.perl.service && sudo sysctl -p"
